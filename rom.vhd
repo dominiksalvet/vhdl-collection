e9c8a45e8f3c9e4d3b3f302f25eb93ea32c78dc3
Dominik Salvet <dominik.salvet@gmail.com>
2018-02-15 22:53:10 +0100

    Create comments in rom.vhd

--- rom.vhd
+++ rom.vhd
@@ -1,3 +1,19 @@
+-------------------------------------------------------------------------------
+-- Standard:    VHDL-1993
+-- Platform:    any
+-- Dependecies: none
+-------------------------------------------------------------------------------
+-- Description:
+--     Generic implementation of single port sychronous ROM memory.
+-------------------------------------------------------------------------------
+-- Comments:
+--     1. Since there is a read enable signal, data_out output will be
+--        implemented as register.
+--     2. The module can be implemented as block memory, if the target platfrom
+--        supports it.
+-------------------------------------------------------------------------------
+
+
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
@@ -5,61 +21,66 @@ use ieee.numeric_std.all;
 
 entity rom is
     generic (
-        ADDR_WIDTH : positive;
-        DATA_WIDTH : positive
-        );
+        ADDR_WIDTH : positive; -- bit width of rom address bus
+        DATA_WIDTH : positive -- bit width of rom data bus
+    );
     port (
-        clk : in std_logic;
-
-        re       : in  std_logic;
-        addr     : in  std_logic_vector(ADDR_WIDTH - 1 downto 0);
-        data_out : out std_logic_vector(DATA_WIDTH - 1 downto 0)
-        );
+        clk : in std_logic; -- clock signal
+        
+        re       : in  std_logic; -- read enable
+        addr     : in  std_logic_vector(ADDR_WIDTH - 1 downto 0); -- address bus
+        data_out : out std_logic_vector(DATA_WIDTH - 1 downto 0) -- output data bus
+    );
 end entity rom;
 
 
 architecture rtl of rom is
-
-    type rom_array_t is array((2 ** ADDR_WIDTH) - 1 downto 0) of std_logic_vector(DATA_WIDTH - 1 downto 0);
-    -- rom initialization
-    signal rom_array : rom_array_t := (
+    
+    -- definition of memory type
+    type memory_t is array((2 ** ADDR_WIDTH) - 1 downto 0) of
+        std_logic_vector(DATA_WIDTH - 1 downto 0);
+    -- accessible memory signal, rom initialization
+    signal memory : memory_t := (
         others => (others => 'U')
         );
-
+    
 begin
-
-    rom_array_read : process(clk)
+    
+    -- Inputs:  clk, re, addr, memory
+    -- Outputs: data_out
+    -- Purpose: Memory read mechanism description.
+    memory_read : process (clk)
     begin
         if (rising_edge(clk)) then
             if (re = '1') then
-                data_out <= rom_array(to_integer(unsigned(addr)));
+                data_out <= memory(to_integer(unsigned(addr)));
             end if;
         end if;
-    end process rom_array_read;
-
+    end process memory_read;
+    
 end architecture rtl;
 
 
----------------------------------------------------------------------------------
+-------------------------------------------------------------------------------
 -- MIT License
 --
 -- Copyright (c) 2016-2018 Dominik Salvet
 --
 -- Permission is hereby granted, free of charge, to any person obtaining a copy
--- of this software and associated documentation files (the "Software"), to deal
--- in the Software without restriction, including without limitation the rights
--- to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
--- copies of the Software, and to permit persons to whom the Software is
+-- of this software and associated documentation files (the "Software"), to
+-- deal in the Software without restriction, including without limitation the
+-- rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+-- sell copies of the Software, and to permit persons to whom the Software is
 -- furnished to do so, subject to the following conditions:
 --
--- The above copyright notice and this permission notice shall be included in all
--- copies or substantial portions of the Software.
+-- The above copyright notice and this permission notice shall be included in
+-- all copies or substantial portions of the Software.
 --
 -- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 -- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 -- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
 -- AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
--- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
--- OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
--- SOFTWARE.
----------------------------------------------------------------------------------
+-- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+-- FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+-- IN THE SOFTWARE.
+-------------------------------------------------------------------------------
