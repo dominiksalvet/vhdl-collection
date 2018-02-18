4a4a2cfa3a6eb541e2c86620b67a13592c03e647
Dominik Salvet <dominik.salvet@gmail.com>
2018-02-18 20:48:24 +0100

    Remake clk_divider.vhd
    One clock mode was removed, clk_div was renamed to freq_div and it's
    data type is now positive instead of direct std_logic_vector. Then
    COUNTER_WIDTH was removed too. Instead of that there was add
    FREQ_DIV_MAX_VALUE as a way to limit sizes of internal registers. The
    clk_divider now uses more local process variables rather than previously
    used architecture global signals.

--- clk_divider.vhd
+++ clk_divider.vhd
@@ -5,10 +5,13 @@
 -------------------------------------------------------------------------------
 -- Description:
 --     This source file represents a generic implementation of a clock divider.
+--     It supports to dynamically change frequency divisor, including 1 value.
 -------------------------------------------------------------------------------
 -- Comments:
---     1. Each clk_out period, clk_div is stored internally and changes on this
---        input are not propaged anywhere until the next clk_out period.
+--     1. Period of output clk_out starts with '1' value, followed by '0'.
+--     2. When it is not possible to perform clock frequency division without
+--        a remainder, the clk_out will have '1' value one clk period shorter
+--        than '0' value per clk_out period.
 -------------------------------------------------------------------------------
 
 
@@ -19,58 +22,68 @@ use ieee.numeric_std.all;
 
 entity clk_divider is
     generic (
-        ONE_CLK_MODE  : boolean; -- when true, clk_out has '1' value only for one clock signal
-        COUNTER_WIDTH : positive -- bit widht of an internal clock counter
-    ); 
+        FREQ_DIV_MAX_VALUE : positive -- maximum available frequency divisor value
+    );
     port (
         clk : in std_logic; -- input clock signal
         rst : in std_logic; -- reset signal
         
-        -- clk_div: clock is divided by value of this signal
-        clk_div : in  std_logic_vector(COUNTER_WIDTH - 1 downto 0);
-        clk_out : out std_logic -- final output clock
+        -- freq_div: clk frequency is divided by value of this number, clk_out=clk/freq_div
+        freq_div : in  positive range 1 to FREQ_DIV_MAX_VALUE;
+        clk_out  : out std_logic -- final output clock
     );
 end entity clk_divider;
 
 
 architecture rtl of clk_divider is
     
-    signal counter : std_logic_vector(COUNTER_WIDTH - 1 downto 0); -- internal clock counter
+    signal use_direct_clk : std_logic;
     
-    signal clk_reg : std_logic; -- clk_out buffer
+    signal divided_clk : std_logic;
     
 begin
     
-    clk_out <= clk_reg;
+    clk_out <= clk when use_direct_clk = '1' else divided_clk;
     
-    -- Inputs:  clk, rst, clk_div, counter, clk_reg
-    -- Outputs: counter, clk_reg
-    -- Purpose: Implementation of the clk signal counter. The counter uses decrementing.
-    count_clk : process (clk)
+    -- Inputs:  clk, freq_div
+    -- Outputs: use_direct_clk
+    -- Purpose: Check for the input when the frequency division by 1 is required.
+    direct_clk_checking : process (clk)
     begin
         if (rising_edge(clk)) then
-            if (rst = '1') then
-                counter <= clk_div;
-                clk_reg <= '0';
+            if (freq_div = 1) then
+                use_direct_clk <= '1';
+            else
+                use_direct_clk <= '0';
+            end if;
+        end if;
+    end process direct_clk_checking;
+    
+    -- Inputs:  clk, rst, freq_div
+    -- Outputs: clk_out
+    -- Purpose: Perform clk requency division by counting and create the final clk_out signal.
+    divide_clk_freq : process (clk)
+        -- freq_div_reg: register to store internally freq_div value in a time
+        variable freq_div_reg : positive range 1 to FREQ_DIV_MAX_VALUE;
+        variable clk_counter  : positive range 1 to FREQ_DIV_MAX_VALUE; -- internal clk counter
+    begin
+        if (rising_edge(clk)) then
+            -- need to reset the clk_counter and begin the new clk_out period
+            if (rst = '1' or clk_counter = freq_div_reg) then
+                divided_clk  <= '1';
+                freq_div_reg := freq_div;
+                clk_counter  := 1;
             else
                 
-                if (counter = (COUNTER_WIDTH - 1 downto 0 => '0')) then -- decreased to 0
-                    counter <= clk_div; -- a new value is assigned to the counter
-                    if (ONE_CLK_MODE) then
-                        clk_reg <= '1';
-                    else
-                        clk_reg <= not clk_reg;
-                    end if;
-                else -- perform one counting step
-                    counter <= std_logic_vector(unsigned(counter) - 1);
-                    if (ONE_CLK_MODE) then
-                        clk_reg <= '0';
-                    end if;
+                if (clk_counter = (freq_div_reg / 2)) then -- half of the clk_out period
+                    divided_clk <= '0';
                 end if;
+                
+                clk_counter := clk_counter + 1; -- counting clk rising edges
                 
             end if;
         end if;
-    end process count_clk;
+    end process divide_clk_freq;
     
 end architecture rtl;
 
