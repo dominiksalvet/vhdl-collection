afc55c5b165650d17ab1c41433c45e911acd648c
Dominik Salvet <dominik.salvet@gmail.com>
2018-03-07 01:48:10 +0100

    Update mem_copier.vhd
    The whole internal structure has been rewritten. Now with the code a
    synthetizer can produce better optimized result.
    
    Still need to be solved an issue with copy_addr_count < 4.

--- mem_copier/mem_copier.vhd
+++ mem_copier/mem_copier.vhd
@@ -4,12 +4,12 @@
 --------------------------------------------------------------------------------
 -- Description:
 --     This VHDL description represents a generic memory copier module. It can
---     operate in wide ways of use. It behaves like a very simple DMA module.
+--     operate in wide ways of use. It behaves like a simple DMA module with
+--     two separated memory busses.
 --------------------------------------------------------------------------------
 -- Notes:
 --     1. The copy_en input must have '1' value for all the time of copying the
---        data.
---     2. The memories must have the same bit width of their data buses.
+--        data as the '0' value behaves like synchronous reset signal.
 --------------------------------------------------------------------------------
 
 
@@ -20,90 +20,100 @@ use ieee.numeric_std.all;
 
 entity mem_copier is
     generic (
-        SRC_ADDR_WIDTH : positive;
-        TAR_ADDR_WIDTH : positive;
-        DATA_WIDTH     : positive
-        );
+        SRC_ADDR_WIDTH : positive := 8;
+        TAR_ADDR_WIDTH : positive := 8;
+        DATA_WIDTH     : positive := 8
+    );
     port (
         clk        : in  std_logic;
         copy_en    : in  std_logic; -- in '0' reacts like a reset signal
         copy_cmplt : out std_logic;
-
-        start_src_addr  : in natural range 0 to (2 ** SRC_ADDR_WIDTH) - 1;
-        start_tar_addr  : in natural range 0 to (2 ** TAR_ADDR_WIDTH) - 1;
+        
+        start_src_addr : in natural range 0 to (2 ** SRC_ADDR_WIDTH) - 1;
+        start_tar_addr : in natural range 0 to (2 ** TAR_ADDR_WIDTH) - 1;
         -- assign copy_addr_count to 0 to program the whole target's address space
-        copy_addr_count : in natural range 0 to (2 ** TAR_ADDR_WIDTH) - 1;
-
+        copy_addr_count : in positive range 1 to 2 ** TAR_ADDR_WIDTH;
+        
         src_data_in : in  std_logic_vector(DATA_WIDTH - 1 downto 0);
+        src_re      : out std_logic;
         src_addr    : out std_logic_vector(SRC_ADDR_WIDTH - 1 downto 0);
-
+        
         tar_we       : out std_logic;
         tar_addr     : out std_logic_vector(TAR_ADDR_WIDTH - 1 downto 0);
         tar_data_out : out std_logic_vector(DATA_WIDTH - 1 downto 0)
-        );
+    );
 end entity mem_copier;
 
 
 architecture rtl of mem_copier is
-
-    signal copy_cmplt_reg : std_logic;
-
-    signal copied_count     : std_logic_vector(SRC_ADDR_WIDTH - 1 downto 0);
-    signal copied_count_inc : std_logic_vector(SRC_ADDR_WIDTH - 1 downto 0);
-
-    signal start_src_addr_reg  : natural range 0 to (2 ** SRC_ADDR_WIDTH) - 1;
-    signal start_tar_addr_reg  : natural range 0 to (2 ** TAR_ADDR_WIDTH) - 1;
-    signal copy_addr_count_reg : natural range 0 to (2 ** TAR_ADDR_WIDTH) - 1;
-
-    signal tar_we_reg : std_logic;
-
+    
+    signal src_re_reg   : std_logic;
+    signal src_addr_reg : natural range 0 to (2 ** SRC_ADDR_WIDTH) - 1;
+    
+    signal tar_we_reg   : std_logic;
+    signal tar_addr_reg : natural range 0 to (2 ** TAR_ADDR_WIDTH) - 1;
+    
 begin
-
-    copy_cmplt <= copy_cmplt_reg;
-
-    copied_count_inc <= std_logic_vector(unsigned(copied_count) + 1);
-
-    src_addr <= std_logic_vector(start_src_addr_reg + unsigned(copied_count_inc));
-
+    
+    src_re <= src_re_reg;
+    
+    src_addr <= std_logic_vector(to_unsigned(src_addr_reg, SRC_ADDR_WIDTH));
+    
     tar_we <= tar_we_reg;
-
-    mem_copying : process(clk)
-        variable init_state : std_logic;
+    
+    tar_addr <= std_logic_vector(to_unsigned(tar_addr_reg, TAR_ADDR_WIDTH));
+    
+    mem_copying : process (clk)
+        variable to_copy_count : natural range 0 to 2 ** TAR_ADDR_WIDTH;
+        type state_t is (READ_INIT, READ_WAIT, WRITE_INIT, WRITE);
+        variable state : state_t;
     begin
         if (rising_edge(clk)) then
-            if (copy_en = '0') then
-                copy_cmplt_reg <= '0';
-                tar_we_reg     <= '0';
-                init_state     := '1';
-            else
-
-                if (init_state = '1') then
-                    copied_count        <= (others => '1');
-                    start_src_addr_reg  <= start_src_addr;
-                    start_tar_addr_reg  <= start_tar_addr;
-                    copy_addr_count_reg <= copy_addr_count;
-                    init_state          := '0';
-                else
-                    tar_addr     <= std_logic_vector(start_tar_addr_reg + unsigned(copied_count));
-                    tar_data_out <= src_data_in;
-
-                    copied_count <= copied_count_inc;
-
-                    if (
-                        copied_count = (SRC_ADDR_WIDTH - 1 downto 0 => '0') and
-                        tar_we_reg = '0' and copy_cmplt_reg = '0'
-                        ) then
-                        tar_we_reg <= '1';
-                    elsif (copied_count = std_logic_vector(to_unsigned(copy_addr_count_reg, SRC_ADDR_WIDTH))) then
-                        copy_cmplt_reg <= '1';
-                        tar_we_reg     <= '0';
-                    end if;
+            
+            tar_data_out <= src_data_in;
+            
+            if (src_re_reg = '1') then
+                src_addr_reg <= src_addr_reg + 1;
+                if (to_copy_count = 3) then
+                    src_re_reg <= '0';
                 end if;
-
+            end if;
+            
+            if (tar_we_reg = '1') then
+                tar_addr_reg <= tar_addr_reg + 1;
+                if (to_copy_count = 1) then
+                    copy_cmplt <= '1';
+                    tar_we_reg <= '0';
+                end if;
+                to_copy_count := to_copy_count - 1;
+            end if;
+            
+            if (copy_en = '0') then
+                copy_cmplt <= '0';
+                src_re_reg <= '0';
+                tar_we_reg <= '0';
+                state      := READ_INIT;
+            else
+                
+                case (state) is
+                    when READ_INIT => 
+                        src_re_reg    <= '1';
+                        src_addr_reg  <= start_src_addr;
+                        tar_addr_reg  <= start_tar_addr;
+                        to_copy_count := copy_addr_count;
+                        state         := READ_WAIT;
+                    when READ_WAIT => 
+                        state := WRITE_INIT;
+                    when WRITE_INIT => 
+                        tar_we_reg <= '1';
+                        state      := WRITE;
+                    when others => null;
+                end case;
+                
             end if;
         end if;
     end process mem_copying;
-
+    
 end architecture rtl;
 
 
