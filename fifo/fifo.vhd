cbcf0659cb3165236a008a9d5d5bccc13d3ffcee
Dominik Salvet <dominik.salvet@gmail.com>
2018-03-10 20:36:48 +0100

    Update fifo.vhd
    The first implementation of the module has been coded.

--- fifo/fifo.vhd
+++ fifo/fifo.vhd
@@ -14,11 +14,61 @@ use ieee.numeric_std.all;
 
 
 entity fifo is
+    generic (
+        DEPTH      : positive;
+        DATA_WIDTH : positive
+    );
+    port (
+        clk : in std_logic;
+        rst : in std_logic;
+        
+        we      : in  std_logic;
+        data_in : in  std_logic_vector(DATA_WIDTH - 1 downto 0);
+        full    : out std_logic;
+        
+        re       : in  std_logic;
+        data_out : out std_logic_vector(DATA_WIDTH - 1 downto 0);
+        empty    : out std_logic
+    );
 end entity fifo;
 
 
 architecture rtl of fifo is
+    
+    type mem_t is array(DEPTH - 1 downto 0) of std_logic_vector(DATA_WIDTH - 1 downto 0);
+    signal mem : mem_t;
+    
+    signal wr_index : natural range 0 to DEPTH - 1;
+    signal rd_index : natural range 0 to DEPTH - 1;
+    
 begin
+    
+    full <= '1' when (wr_index + 1) mod DEPTH = rd_index else '0';
+    
+    empty <= '1' when wr_index = rd_index else '0';
+    
+    mem_access : process (clk)
+    begin
+        if (rising_edge(clk)) then
+            if (rst = 1) then
+                wr_index <= 0;
+                rd_index <= 0;
+            else
+                
+                if (we = '1') then
+                    mem(wr_index) <= data_in;
+                    wr_index      <= wr_index + 1;
+                end if;
+                
+                if (re = '1') then
+                    data_out <= mem(rd_index);
+                    rd_index <= rd_index + 1;
+                end if;
+                
+            end if;
+        end if;
+    end process mem_access;
+    
 end architecture rtl;
 
 
