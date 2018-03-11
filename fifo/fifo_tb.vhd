46ddfa70f959ae0efe9d55bb34d466b659ffb4fe
Dominik Salvet <dominik.salvet@gmail.com>
2018-03-11 16:20:27 +0100

    Update FIFO modules
    The fifo.vhd and fifo_tb.vhd has been completed and commented. The
    functionality has been verified by the test bench. A project file has
    been updated to apply these changes.

--- fifo/fifo_tb.vhd
+++ fifo/fifo_tb.vhd
@@ -1,5 +1,9 @@
 --------------------------------------------------------------------------------
 -- Description:
+--     The test bench first fulls up all the FIFO internal memory defined by
+--     INDEX_WIDTH, which is set to 2, so internal capacity is 4 items. Then it
+--     will test the full indicator and read all the items. Then it will verify
+--     all the read data and empty indicator at the end.
 --------------------------------------------------------------------------------
 -- Notes:
 --------------------------------------------------------------------------------
@@ -7,6 +11,7 @@
 
 library ieee;
 use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
 
 use work.fifo; -- fifo.vhd
 
@@ -18,18 +23,18 @@ end entity fifo_tb;
 architecture behavior of fifo_tb is
     
     -- uut generics
-    constant INDEX_WIDTH : positive;
-    constant DATA_WIDTH  : positive;
+    constant INDEX_WIDTH : positive := 2;
+    constant DATA_WIDTH  : positive := 8;
     
     -- uut ports
-    signal clk : std_logic;
-    signal rst : std_logic;
+    signal clk : std_logic := '0';
+    signal rst : std_logic := '0';
     
-    signal we      : std_logic;
-    signal data_in : std_logic_vector(DATA_WIDTH - 1 downto 0);
+    signal we      : std_logic                                 := '0';
+    signal data_in : std_logic_vector(DATA_WIDTH - 1 downto 0) := (others => '0');
     signal full    : std_logic;
     
-    signal re       : std_logic;
+    signal re       : std_logic := '0';
     signal data_out : std_logic_vector(DATA_WIDTH - 1 downto 0);
     signal empty    : std_logic;
     
@@ -51,7 +56,7 @@ begin
             we      => we,
             data_in => data_in,
             full    => full,
-
+            
             re       => re,
             data_out => data_out,
             empty    => empty
@@ -69,6 +74,44 @@ begin
     -- Purpose: Stimulus process.
     stim_proc : process
     begin
+        
+        rst <= '1';
+        wait for CLK_PERIOD; -- intitialize the uut
+        
+        rst <= '0';
+        we  <= '1'; -- write process start
+        wait for CLK_PERIOD;
+        
+        data_in <= std_logic_vector(unsigned(data_in) + 1);
+        wait for CLK_PERIOD;
+        
+        data_in <= std_logic_vector(unsigned(data_in) + 1);
+        wait for CLK_PERIOD;
+        
+        data_in <= std_logic_vector(unsigned(data_in) + 1);
+        wait for CLK_PERIOD;
+        
+        assert (full = '1')
+            report "The full indicator should have '1' value!" severity error;
+        
+        we <= '0';
+        re <= '1';
+        wait for CLK_PERIOD;
+        
+        for i in 0 to 2 loop
+            assert (data_out = std_logic_vector(to_unsigned(i, DATA_WIDTH)))
+                report "Invalid value has been read from the FIFO!" severity error;
+            wait for CLK_PERIOD;
+        end loop;
+        
+        assert (data_out = std_logic_vector(to_unsigned(3, DATA_WIDTH)))
+            report "Invalid value has been read from the FIFO!" severity error;
+        assert (empty = '1')
+            report "The empty indicator should have '1' value!" severity error;
+        
+        re <= '0';
+        wait;
+        
     end process stim_proc;
     
 end architecture behavior;
