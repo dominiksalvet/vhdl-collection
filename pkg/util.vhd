feaf272ac741a3e1e437bb0c1e6a4bac6caa70f4
Dominik Salvet <dominik.salvet@gmail.com>
2018-04-03 22:54:36 +0200

    Update util.vhd
    The contains_only_01 has been implemented as an overloaded variant for
    the std_logic data type.

--- pkg/util.vhd
+++ pkg/util.vhd
@@ -16,6 +16,13 @@ use ieee.std_logic_1164.all;
 package util is
     
     -- Description:
+    --     The function checks the input signal p_SIGNAL and returns false if it contains any other
+    --     value than '1' or '0'. The function is not intended to be synthesized.
+    function contains_only_01 (
+            p_SIGNAL : std_logic -- input standard signal
+        ) return boolean;
+    
+    -- Description:
     --     The function checks the input vector p_VECTOR and returns false if it contains any other
     --     value than '1' or '0' in it's scalar components. The function is not intended to be
     --     synthesized.
@@ -29,11 +36,18 @@ end package util;
 package body util is
     
     function contains_only_01 (
+            p_SIGNAL : std_logic
+        ) return boolean is
+    begin
+        return p_SIGNAL = '0' or p_SIGNAL = '1';
+    end function contains_only_01;
+    
+    function contains_only_01 (
             p_VECTOR : std_logic_vector
         ) return boolean is
     begin
         for i in p_VECTOR'range loop -- check every vector's scalar component
-            if (p_VECTOR(i) /= '0' and p_VECTOR(i) /= '1') then
+            if (not contains_only_01(p_VECTOR(i))) then
                 return false;
             end if;
         end loop;
