d661f30be5056a658144200cca3490b8ea4528bc
Dominik Salvet <dominik.salvet@gmail.com>
2018-04-06 13:03:10 +0200

    Comment to_character, to_string in util_pkg.vhd

--- lib/vhdl_collection/util_pkg.vhd
+++ lib/vhdl_collection/util_pkg.vhd
@@ -3,9 +3,11 @@
 -- Platform: independent
 --------------------------------------------------------------------------------
 -- Description:
---     This package contains basic verification utility functions.
+--     This package contains basic utility functions.
 --------------------------------------------------------------------------------
 -- Notes:
+--     1. Unless stated otherwise, the functions are not intended to be
+--        synthesized.
 --------------------------------------------------------------------------------
 
 
@@ -17,26 +19,30 @@ package util_pkg is
     
     -- Description:
     --     The function returns true only when the input signal p_SIGNAL has '0' or '1' value, false
-    --     is returned otherwise. The function is not intended to be synthesized.
+    --     is returned otherwise.
     function contains_01 (
             p_SIGNAL : std_logic -- input standard logic signal
         ) return boolean;
     
     -- Description:
     --     The function returns true only when all the scalar components of the input vector
-    --     p_VECTOR have '0' or '1' value, false is returned otherwise. The function is not intended
-    --     to be synthesized.
+    --     p_VECTOR have '0' or '1' value, false is returned otherwise.
     function contains_01 (
             p_VECTOR : std_logic_vector -- input standard logic vector
         ) return boolean;
     
+    -- Description:
+    --     Returns the std_logic parameter's character representation.
     function to_character (
-            p_SIGNAL : std_logic
-        ) return character;
+            p_SIGNAL : std_logic -- input standard logic signal
+        ) return character; -- final character
     
+    -- Description:
+    --     Returns the std_logic_vector parameter's string representation. It always respects the
+    --     vector's range definition.
     function to_string (
-            p_VECTOR : std_logic_vector
-        ) return string;
+            p_VECTOR : std_logic_vector -- input standard logic vector
+        ) return string; -- final string
     
 end package util_pkg;
 
@@ -59,15 +65,16 @@ package body util_pkg is
                 return false;
             end if;
         end loop;
+
         return true;
     end function contains_01;
     
     function to_character (
             p_SIGNAL : std_logic
         ) return character is
-        variable v_character : character;
+        variable v_character : character; -- final character
     begin
-        case (p_SIGNAL) is
+        case (p_SIGNAL) is -- convert the std_logic to the character
             when 'U' => 
                 v_character := 'U';
             when 'X' => 
@@ -87,20 +94,23 @@ package body util_pkg is
             when '-' => 
                 v_character := '-';
         end case;
+
         return v_character;
     end function to_character;
     
     function to_string (
             p_VECTOR : std_logic_vector
         ) return string is
-        variable v_string : string(p_VECTOR'low + 1 to p_VECTOR'high + 1);
+        -- as a range it can't be used p_VECTOR'range as it allows one of the boundaries to be 0
+        variable v_string : string(p_VECTOR'low + 1 to p_VECTOR'high + 1); -- final string
     begin
-        if (p_VECTOR'ascending) then
+        if (p_VECTOR'ascending) then -- range of p_VECTOR is defined with "to"
             for i in v_string'range loop
-                v_string(i) := to_character(p_VECTOR(i - 1));
+                v_string(i) := to_character(p_VECTOR(i - 1)); -- calling to_character on every bit
             end loop;
-        else
+        else -- range of p_VECTOR is defined with "downto"
             for i in v_string'range loop
+                -- calling to_character on every bit
                 v_string(i) := to_character(p_VECTOR(p_VECTOR'high - i + 1));
             end loop;
         end if;
