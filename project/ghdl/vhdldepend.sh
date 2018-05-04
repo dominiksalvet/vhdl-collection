3565eaec3f0910b934b72f1c3d4fd826d4ba739f
Dominik Salvet <dominik.salvet@gmail.com>
2018-05-04 22:40:20 +0200

    Add automatic dependency solving
    The very first version of automatic VHDL dependency solve solution has
    been created. The core of the VHDL dependency solving is vhdldepend
    shell script, which uses sed.
    
    The vhdldepend expect two parameters, the first one - path to file to
    get dependency from, the second one - global prefix for each target and
    dependency in created makefile. As an output it will created a
    makefile-compatible rules that describes the input file dependencies.
    That file can be later used in main makefile with include statement to
    include newly generated rules for each file.
    
    Manually generated file deps.mk has been relocated to the dep/dep.mk
    location and will be kept until final version of VHDL automatic
    dependency solving will be released.

---
+++ project/ghdl/vhdldepend.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+target=$(echo "$1" | sed -e 's|.*/||;s/\.[^\.]*$/.o/;s|^|'"$2"'|;')
+
+command1='/^\s*use/!d;s/use//;s/\s*//g;/^ieee\|^std/d;s/\;$//;s/\.all$//;s/^.*\.//;s/^.*/&.o/;s|^|'"$2"'|;'
+command2=':next;$bdone;N;bnext;:done;s/\n/ /g;s|^|'"$target"': |;'
+
+sed -e "$command1" "$1" |
+sed -e "$command2"
